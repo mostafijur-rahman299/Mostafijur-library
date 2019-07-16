c8cc1f168d9360d95feb3d2c47adc7b1ae7e0756
Md.Mostafijur Rahman <39603091+mostafijurrahman299@users.noreply.github.com>
2019-07-16 10:50:06 +0600

    c v and i v added

--- Python/2.Part-Two/Oop/Class variabe and Instance variable.py
+++ Python/2.Part-Two/Oop/Class variabe and Instance variable.py
@@ -13,9 +13,12 @@ alien=Alien('Maven')
 alien2=Alien('Oven')
 alien.name='sajib'
 print(alien.name,alien2.name)
+
 alien.legs=78
 print(alien.legs,alien2.legs)
+
 Alien.legs=100
 print(alien.legs,alien2.legs)
+
 alien.__class__.legs=200
 print(alien.legs,alien2.legs)
