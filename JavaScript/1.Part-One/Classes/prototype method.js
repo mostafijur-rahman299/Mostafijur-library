77f04fa6f82b78e05346eeebc60e5da583f4f17c
Md.Mostafijur Rahman <39603091+mostafijur-rahman299@users.noreply.github.com>
2020-06-29 13:05:05 +0600

    Update prototype method.js

--- JavaScript/1.Part-One/Classes/prototype method.js
+++ JavaScript/1.Part-One/Classes/prototype method.js
@@ -3,10 +3,12 @@ class Account{
 		this.firstname = firstName
 		this.lastname = lastName
 	}
-	get_fullName(){
-		return this.firstname + this.lastname
-	}
 }
 
 var obj = new Account('sajib ','mahmud')
+
+Accont.prototype.get_fullName = function(){
+	return this.firstname + this.lastname
+}
+
 obj.get_fullName()
